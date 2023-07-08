c85122819e82799493feabcec59f5b0b6f0c78a5
Teresavz <202103856@est.umss.edu>
2023-07-07 22:02:18 -0400

    bd.

--- ProyectoEncriptacion/ProyectoEncriptacion/Form1.cs
+++ ProyectoEncriptacion/ProyectoEncriptacion/Form1.cs
@@ -10,15 +10,20 @@ using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
 using System.Security.Cryptography;
-//using System.Security.Permissions;
+using Npgsql;
 
 namespace ProyectoEncriptacion
 {
     public partial class Form1 : Form
     {
+        private string connectionString;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Configura la cadena de conexión
+            connectionString = "Server=localhost;Port=5432;Database=encriptacion;User Id=postgres;Password=Pincholin7;";
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -38,14 +43,14 @@ namespace ProyectoEncriptacion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 //Abre una ventana mostrando el contenido del archivo
                 string archivo = openFileDialog1.FileName;
                 Process proceso = new Process();
                 proceso.StartInfo.FileName = archivo;
                 proceso.Start();
-                
+
                 textBox2.Text = openFileDialog1.FileName;
             }
         }
@@ -62,11 +67,11 @@ namespace ProyectoEncriptacion
 
                 //Agregacion de la extension al archivo destino
                 string destinationFileWithExtension = Path.ChangeExtension(destinationFilePath, fileExtension);
-                
+
                 // Copia el archivo seleccionado a la ubicación de destino
                 //File.Copy(sourceFilePath, destinationFileWithExtension); 
-               // MessageBox.Show("El archivo se guardó exitosamente.");
-                
+                // MessageBox.Show("El archivo se guardó exitosamente.");
+
                 textBox3.Text = destinationFileWithExtension;
             }
         }
@@ -79,10 +84,27 @@ namespace ProyectoEncriptacion
             string outputFileWithExtension = Path.ChangeExtension(outputFile, fileExtension);
 
             string password = "";
-            EncryptFile(inputFile, outputFileWithExtension, password);
 
-            MessageBox.Show("Archivo encriptado y guardado exitosamente.");
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexión exitosa");
 
+                    // Realiza operaciones en la base de datos aquí
+
+                    EncryptFile(inputFile, outputFileWithExtension, password);
+
+                    MessageBox.Show("Archivo encriptado y guardado exitosamente.");
+
+                    connection.Close();
+                }
+                catch (NpgsqlException ex)
+                {
+                    Console.WriteLine("Error al conectar con la base de datos: " + ex.Message);
+                }
+            }
         }
 
         public static void EncryptFile(string inputFile, string outputFileWithExtension, string password)
@@ -115,7 +137,6 @@ namespace ProyectoEncriptacion
                 }
             }
         }
-
 
         private void button4_Click(object sender, EventArgs e)
         {
