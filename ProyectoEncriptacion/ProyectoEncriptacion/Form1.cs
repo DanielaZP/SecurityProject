d121fcc9a974184959966cc23c30c7f0f816b79d
Teresavz <202103856@est.umss.edu>
2023-07-08 17:58:58 -0400

    .

--- ProyectoEncriptacion/ProyectoEncriptacion/Form1.cs
+++ ProyectoEncriptacion/ProyectoEncriptacion/Form1.cs
@@ -140,7 +140,7 @@ namespace ProyectoEncriptacion
                 MessageBox.Show("El archivo desencriptado no existe.");
             }
         }
-
+         //
          */
 
         public static void EncryptFile(string inputFile, string outputFileWithExtension, string password)
