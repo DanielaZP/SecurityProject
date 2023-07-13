05b4fa16cb6827f1f4e1d75fd542a1ac4baa0130
Teresavz <98954265+Teresavz@users.noreply.github.com>
2023-07-13 13:45:42 +0000

    prueba extns

--- conexion.php
+++ conexion.php
@@ -1,19 +1,13 @@
-<!-- Aqui tiene que estar la conexion con la bd, pero aun no encuentro su extrension xd-->
 <?php
 $host = 'localhost';
-$port = '5432';
 $dbname = 'encriptacion';
 $user = 'postgres';
 $password = 'Pincholin7';
 
-$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
-
-$conn = pg_connect($connection_string);
-if ($conn === false) {
-  echo "Error al conectar a la base de datos";
-  exit;
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    echo 'Conectado a la base de datos';
+} catch (PDOException $e) {
+    echo 'Error al conectar a la base de datos: ' . $e->getMessage();
 }
-
-
-pg_close($conn);
 ?>
