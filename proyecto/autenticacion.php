11477c9bc746e2aabfd7e3db959cfa0bf9f50d95
Teresavz <202103856@est.umss.edu>
2023-07-14 13:52:30 -0400

    autenticacionp1

---
+++ proyecto/autenticacion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> autenticacion cuenta/title>
+    <link rel="stylesheet"  href="style.css"> 
+  </head>
+
+  <body>
+     <form class="registro">
+     
+      <h1>aca hacemos le 2fa </h1>
+   
+      </div>
+    
+     </form>
+    
+  </body>
+</html>
