4e1a7662bf9999e25acbee75e91de662060a2e02
Teresavz <202103856@est.umss.edu>
2023-07-15 03:24:26 -0400

    ParteDisenoInicio

--- proyecto/inicio.php
+++ proyecto/inicio.php
@@ -1,3 +1,17 @@
-<?php
-echo 'inicio';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>INICIO</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+     <form class="registro">
+        <img src="Imagenes/Logo.jpg" class="avatar">
+      <h1>ACA INGRESAREMOS LOS ARCHIVOS A ENCRIPTAR O DESNCRIPTAR DE USUARIO</h1>
+      
+     </form>
+    
+  </body>
+</html>
