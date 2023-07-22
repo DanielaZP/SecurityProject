940d7297fc57bdee693488a781d86b0036ade824
unknown <202103159@est.umss.edu>
2023-07-22 15:57:56 -0400

    Funcionamiento de encriptacion y desencriptacion

--- proyecto/inicio.php
+++ proyecto/inicio.php
@@ -5,25 +5,6 @@
     <title>Aplicación | Encriptación y Desencriptación AES</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
-    <style>
-        /* Estilos del panel lateral */
-        .panel-lateral {
-            position: fixed;
-            top: 0;
-            right: 0;
-            width: 200px;
-            height: 100%;
-            background-color: #b1d3e4;
-            padding: 20px;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-        }
-
-        .panel-lateral button {
-            margin-bottom: 10px;
-        }
-    </style>
 </head>
 <body>
     <form class="registro">
@@ -52,14 +33,6 @@
             </div>
         </div>
     </form>
-                    
-    <!-- Panel lateral con opciones -->
-    <div class="panel-lateral">
-        <button class="button" onclick="cerrarSesion()">
-            Cerrar sesión
-        </button>
-        <!-- Otras opciones del panel lateral -->
-    </div>
 
     <script>
         function encriptar() {
@@ -108,10 +81,6 @@
                 }
             };
             reader.readAsText(file);
-        }
-        function cerrarSesion() {
-            // Redireccionar a la página "login.php" al hacer clic en "Cerrar sesión"
-            window.location.href = "login.php";
         }
     </script>
 </body>
